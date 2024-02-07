446c49d6b757e766c54e9d2136cda2b48a42014e
Ifty Zubaer <76530097+iftyzubaer@users.noreply.github.com>
2024-02-07 23:10:32 +0600

    Update countdown js

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('January 1, 2023 00:00:00').getTime();
+    const countDate = new Date('January 1, 2025 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
